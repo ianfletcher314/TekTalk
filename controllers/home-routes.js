7fb7d0147b5092704a4715c006ba8279d9de3571
Ian Fletcher <ianfletcher314@gmail.com>
2021-05-03 16:14:37 -0500

    controllers module has been modified with home and api routs sections and is connected now to server.js. now i must create and connect the utils/helpers section

---
+++ controllers/home-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+// const { Gallery, Painting } = require('../models');
+// check 19 ins_middleware for get querries like this!!
+
+// GET all galleries for homepage
+// router.get('/', async (req, res) => {
+//   try {
+//     const dbGalleryData = await Gallery.findAll({
+//       include: [
+//         {
+//           model: Painting,
+//           attributes: ['filename', 'description'],
+//         },
+//       ],
+//     });
+
+//     const galleries = dbGalleryData.map((gallery) =>
+//       gallery.get({ plain: true })
+//     );
+
+//     res.render('homepage', {
+//       galleries,
+//       loggedIn: req.session.loggedIn,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+module.exports = router;
