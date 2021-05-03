f08e3e8aec61c1f807b7d645745a964a17d1c988
Ian Fletcher <ianfletcher314@gmail.com>
2021-05-03 17:37:12 -0500

    tried to write seed code but realized i needed to set up the releationships between post comment and user so I created the realtionships in the models section. now I will try to write out the seeds section and validate that the relationships are written correctly in models. hopefully this will alow us to seed tables simply by running an npm run seeds command that i will need to add to the package.json

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    post_text: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Post',
+  }
+);
+
+module.exports = Post;
