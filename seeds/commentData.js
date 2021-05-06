7cbb7aac8e50af6d954fc1cf29ea9fe484874804
Ian Fletcher <ianfletcher314@gmail.com>
2021-05-06 18:00:12 -0500

    added another post and another comment to the seed data proved that the htlm is dynamically generated by how many posts there are. now I need to querry for comments and then make usre that they are rendering as well.

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -6,6 +6,11 @@ const commentData = [
     comment_text: 'Test Title',
     post_id: 1,
   },
+  {
+    id: 2,
+    comment_text: 'Test Title2',
+    post_id: 2,
+  },
   
 ];
 
--- seeds/postData.js
+++ seeds/postData.js
@@ -7,6 +7,12 @@ const postData = [
     post_text: 'text text test test',
     user_id: 1,
   },
+  {
+    id: 2,
+    post_title: 'Test Title2',
+    post_text: 't2 2 2 2 2 ',
+    user_id: 1,
+  },
   
 ];
 
