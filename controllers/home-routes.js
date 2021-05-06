14ea7565697421533d0d0e795d1db69f2cac3ced
Ian Fletcher <ianfletcher314@gmail.com>
2021-05-06 15:30:09 -0500

    figured out how to bring information from server into bootstrap. now I need to make the page dynamicaly render post elements for each post in the database using partial and #each loops.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,76 @@
 const router = require('express').Router();
-// const { Gallery, Painting } = require('../models');
+const { Post, User, Comment } = require('../models');
 // check 19 ins_middleware for get querries like this!!
 
-// GET all galleries for homepage
+// GET all posts for homepage
 // router.get('/', async (req, res) => {
 //   try {
-//     const dbGalleryData = await Gallery.findAll({
+//     const dbPostData = await Post.findAll({
 //       include: [
 //         {
-//           model: Painting,
-//           attributes: ['filename', 'description'],
+//           model: Post,
+//           attributes: ['id','post_title', 'post_text','user_id','created_at','updated_at'],
 //         },
 //       ],
 //     });
 
-//     const galleries = dbGalleryData.map((gallery) =>
-//       gallery.get({ plain: true })
+//     const posts = dbPostData.map((post) =>
+//       post.get({ plain: true })
 //     );
 
-//     res.render('homepage', {
-//       galleries,
-//       loggedIn: req.session.loggedIn,
+//     res.render('home', {
+//       posts,
+//     //   loggedIn: req.session.loggedIn,
 //     });
 //   } catch (err) {
 //     console.log(err);
 //     res.status(500).json(err);
 //   }
 // });
+console.log("routerworking")
+router.get('/', async (req,res) => {
+    try {
+        const dbPostData = await Post.findAll({
+          include: [
+            {
+              model: User,
+              attributes: ['username'],
+            },
+          ],
+        });
 
+    
+        const post = dbPostData.map((post) =>
+        post.get({ plain: true }),
+          
+        );
+        console.log(post)
+    
+        res.render('home', {
+             post
+        //   loggedIn: req.session.loggedIn,
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+})
+router.get('/dashboard', (req,res) => {
+
+  res.render('dashboard')
+})
+router.get('/comment', (req,res) => {
+
+  res.render('comment')
+})
+router.get('/newpost', (req,res) => {
+
+  res.render('newpost')
+})
+router.get('/home', (req,res) => {
+
+  res.render('home')
+})
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,34 +8,19 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
+const hbs = exphbs.create({helpers})
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // sets up handlebars
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars',hbs.engine ); //exphbs({defaultLayout: 'main'})
 app.set('view engine', 'handlebars')
 
 // routing for handlebars 
-app.get('/', (req,res) => {
-
-  res.render('home')
-})
-app.get('/dashboard', (req,res) => {
-
-  res.render('dashboard')
-})
-app.get('/comment', (req,res) => {
-
-  res.render('comment')
-})
-app.get('/newpost', (req,res) => {
-
-  res.render('newpost')
-})
-app.get('/home', (req,res) => {
-
-  res.render('home')
-})
+app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
