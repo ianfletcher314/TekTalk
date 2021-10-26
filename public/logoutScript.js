1a9af897f50540ea72699d856f71cd6a5f02ea25
Ian Fletcher <ianfletcher314@gmail.com>
2021-10-26 16:48:51 -0500

    commented logout script with directions on how to swithc to API route

--- public/logoutScript.js
+++ public/logoutScript.js
@@ -1,6 +1,7 @@
 const logoutPlease = async(event) => {
     console.log("we are logging out")
     event.preventDefault()
+  // this is coming from the home-routes.js file and should eventually come from the user-routes.js file ie api/logout
     fetch('/logout',{
         method: 'POST',
     })
