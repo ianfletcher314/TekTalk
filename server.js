9c55c743e23b2a919cac51b1e2ba1a018b3749ec
Ian Fletcher <ianfletcher314@gmail.com>
2021-11-24 14:06:18 -0600

    added prettier formatting to help the code look better

--- server.js
+++ server.js
@@ -1,42 +1,40 @@
-const path = require('path')
-const express = require('express')
-const session = require('express-session')
-const exphbs = require('express-handlebars')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const nocache = require('nocache')
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const nocache = require("nocache");
 
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
-const routes = require('./controllers')
-const sequelize = require('./config/connection')
-const helpers = require('./utils/helpers')
-
-const hbs = exphbs.create({helpers})
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // sets up handlebars
-app.use(express.json())
-app.use(nocache())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
-app.engine('handlebars',hbs.engine ); //exphbs({defaultLayout: 'main'})
-app.set('view engine', 'handlebars')
+app.use(express.json());
+app.use(nocache());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", hbs.engine); //exphbs({defaultLayout: 'main'})
+app.set("view engine", "handlebars");
 
 const sess = {
   secret: "pingpong",
-  cookie: {maxAge:300000},
+  cookie: { maxAge: 300000 },
   resave: false,
   saveUnitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
-}
-app.use(session(sess))
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
 
-// routing for handlebars 
+// routing for handlebars
 app.use(routes);
 
-
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
